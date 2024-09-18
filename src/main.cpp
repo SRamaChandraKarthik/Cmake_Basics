0d15a8c67faeaa79603ec13a96ba46b0b7075aaa
rama chandra <rck>
2024-09-18 19:03:12 +0530

    Karthik from windows

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b)
+{
+
+    return a + b;
+
+}
+
+int main()
+{
+    std::cout << "The sum is " << add(10 , 10) << std::endl;
+    return 0;
+}
